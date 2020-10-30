58fd4c8c16bd54e675653706b1d2e28ffc828f06
NganPh <ngantuyetpham99@gmail.com>
2020-10-30 22:30:23 +0100

    Updates

---
+++ precache-manifest.a6ab912fee4580032f66a003616b13c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543108ba0c885eee6a0eb8d169835ff6",
+    "url": "/What-to-eat/index.html"
+  },
+  {
+    "revision": "937ccf16f87e11041739",
+    "url": "/What-to-eat/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d65f737b5f66eadd1e6e",
+    "url": "/What-to-eat/static/css/main.012c0829.chunk.css"
+  },
+  {
+    "revision": "937ccf16f87e11041739",
+    "url": "/What-to-eat/static/js/2.2fba31c0.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/What-to-eat/static/js/2.2fba31c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65f737b5f66eadd1e6e",
+    "url": "/What-to-eat/static/js/main.74c39cc5.chunk.js"
+  },
+  {
+    "revision": "043195e3b4642e8a6f4b",
+    "url": "/What-to-eat/static/js/runtime-main.0d422af1.js"
+  }
+]);
